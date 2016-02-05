15c791a57c2fdfb95f6497d6f652fc8bcac873d6
duquanyong <duquan.yong@163.com>
2016-02-05 14:57:20 +0800

    加入显示天气信息的功能。

--- gen/com/coolweather/app/R.java
+++ gen/com/coolweather/app/R.java
@@ -1077,7 +1077,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int home=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int refresh=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,12 +1093,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
+        public static final int city_name=0x7f05003f;
         public static final int collapseActionView=0x7f05000d;
+        public static final int current_date=0x7f050043;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1119,7 +1123,9 @@ containing a value of this type.
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
+        public static final int publish_text=0x7f050041;
         public static final int radio=0x7f05002f;
+        public static final int refresh_weather=0x7f050040;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1136,12 +1142,17 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int switch_city=0x7f05003e;
         public static final int tabMode=0x7f050002;
+        public static final int temp1=0x7f050045;
+        public static final int temp2=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int title_text=0x7f05003c;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int weather_desp=0x7f050044;
+        public static final int weather_info_layout=0x7f050042;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1196,6 +1207,7 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int choose_area=0x7f030018;
         public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int weather_layout=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
